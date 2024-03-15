58d0fafb60f56e22660376274f8ac6ad1c213d55
horabaixa <horabaixa@daw1-34>
2024-03-15 15:27:54 +0100

    PRIMERA SUBIDA DEL PROYECTO AL REPOSITORIO LOCAL

---
+++ DcordobaProyecto/src/dCordobaPaquete/DcordobaClase.java
@@ -0,0 +1,8 @@
+package dCordobaPaquete;
+
+public class DcordobaClase {
+	
+	public static void main(String[] args) {
+		System.out.println("El alumno de esta clase es Daniel Córdoba Caballero ");
+	}
+}
---
+++ DcordobaProyecto/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module DcordobaProyecto {
+}
